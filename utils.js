b1d0e0a72eba5d7898f898f8d9034810456f107e
Howard Grimberg <hgrimberg@ku.edu>
2013-11-04 01:06:09 -0600

    Checkpoint 4 Nov 2013. Still a WIP

--- utils.js
+++ utils.js
@@ -1,6 +1,5 @@
 var utils = {
-
-    getFileFieldAsString:function(target, nextfunc){
+    getFileFieldAsString:function(target){
             var file  = target[0].files[0];
             var fileReader = new FileReader();
             var resultString;
@@ -14,7 +13,8 @@ var utils = {
             }else{
                 fileReader.onloadend = function(){                
                     resultString = fileReader.result;
-                    nextfunc(resultString.trim());                   
+                    $(document).trigger('inputLoaded', resultString.trim());
+                
                 };
 
                 fileReader.readAsText(file);
@@ -30,14 +30,15 @@ var utils = {
         var preProcess = base.split('\n');
 
         var clean = _.map(preProcess,function(e){ return e.trim()});
+       
+        // TODO Reorganize this so it uses good variable names.
         preProcess = clean;
-       console.log(preProcess); 
+
         var registerStartIndex = _.indexOf(preProcess,'REGISTERS');
 
         var memoryStartIndex = _.indexOf(preProcess,'MEMORY');
 
         var codeStartIndex = _.indexOf(preProcess,'CODE');
-        console.log(codeStartIndex);
         var registers = preProcess.slice(registerStartIndex+1,memoryStartIndex);
 
 
@@ -45,16 +46,106 @@ var utils = {
 
         var code = preProcess.slice(codeStartIndex+1);
 
-        console.log(registers);
-        console.log(memory);
-        console.log(code);
+        var r = {}
+        var m = {}
+        var ins = {}
+
+        _.each(registers, function(elem){
+               var reg =  elem.split(" ");
+               var regNum = reg[0].replace('R','');
+               var regVal = reg[1];
+               r[regNum] = regVal;
+        });
+
+
+
+        _.each(memory, function(elem){
+               var mem  =  elem.split(" ");
+               m[mem[0]] = mem[1];
+        });
+
+       var iCount  = 0;
+       var iList   = 0;
+       var labelMap = {};
+
+
+        _.each(code, function(line){
+            
+            //Check For Branch Target
+            
+            if (_.indexOf(line,':') !== -1){
+                var dissect = line.split(':');
+                var sanitized = _.map(dissect,function(e){ return e.trim()});
+                
+                var lable = sanitized[0];
+                
+                labelMap[label] = iCount;
+                line = sanitized[1];
+                  
+            }
+              
+            var decode = line.split(" ");
+            
+            var inst = decode[0].trim();
+            
+            var srcd = decode[1].trim().split(',');
+
+
+            
+
+            //Check For Immediate
+            
+          // if(_.indexof(line,
+           
+
+
+            // Increment Instruction Count
+            iCount++;
+        });
+
+
+
+        var parsed = {"registers":r,"memory":m,"code":code};
+
+
+
+    return parsed;
 
         
+    
 
 
+    },
 
-    } 
 
+    getInstruction: function(line){
+        return function(){
+            
+            var decode = line.split(" ");
+            var inst = decode[0].trim(); 
+            var srcd = decode[1].trim().split(',');
+
+            switch(inst){
+                case "BNEZ":
+                    return new Instruction("R","BNEZ",srcd[0], srcd[2],null,true);
+                    
+                    break;
+                case "DADD":
+
+                    return new Instruction("R","BNEZ",srcd[0], srcd[2],null,true);
+                    break;
+                case "LD":
+                    break;
+                case "SW":
+                    break;
+                case "SUB":
+                    break;
+            
+
+
+            }
+        };
+    }
 
 
 }
